f2b04f37fd8239442b4f7673f04d511bb81e2960
Vasseur Nicolas <vasseur@b11p3.fil.univ-lille1.fr>
2016-09-21 13:36:52 +0200

    classe Conversion & Creneau

--- TAC-WEBA_TP1/public_html/javascript.js
+++ TAC-WEBA_TP1/public_html/javascript.js
@@ -3,12 +3,13 @@ window.onload = function(){
     document.getElementById("convertir").addEventListener(
         "click",
         { handleEvent : function (event) { 
-            Conversion.handleEvent(event);
+            conversion = new Conversion("icsData", "jsonData");
+            conversion.handleEvent(event);
         }}
     );
 };
 
-Conversion = {
+/*Conversion = {
     
     jsonData:"",
     
@@ -50,4 +51,63 @@ Conversion = {
                 this.jsonData += ",\n";
             }
     }
+};*/
+
+function Conversion(source, cible){
+    this.source = document.getElementById(source);
+    this.cible = document.getElementById(cible);
+    this.creneaux = new Array;
+}
+
+Conversion.prototype.handleEvent = function(event){
+    this.demarrer();
+}
+
+Conversion.prototype.demarrer = function(){
+    var icsData = this.source.value;
+    this.conversion(icsData);
+    this.cible.value = JSON.stringify(this.creneaux);
+    
 };
+
+Conversion.prototype.conversion = function(icsData){
+    var icsData_inArray = icsData.split("BEGIN:VEVENT");
+    icsData_inArray.shift();
+    for(var index = 0; index < icsData_inArray.length; index++){
+        var creneau = new Creneau();
+        var champs = icsData_inArray[index].split(String.fromCharCode(10));
+        champs.shift();
+        for(var indexChamps = 0; indexChamps < champs.length; indexChamps++){
+            this.conversionChamp(creneau, champs[indexChamps]);
+            
+        }
+        this.creneaux.push(creneau);
+    }
+}
+
+Conversion.prototype.conversionChamp = function (creneau, champActuel){
+    
+    if(champActuel.indexOf(":") != 1){
+        var couple = champActuel.split(":");
+        console.log(couple);
+        if(couple[0] == "DTSTART"){
+            creneau.dateDébut = couple[1];
+        }
+        if(couple[0] == "DTEND"){
+            creneau.dateFin = couple[1];
+        }
+        if(couple[0] == "SUMMARY"){
+            creneau.titre = couple[1];
+        }
+        if(couple[0] == "LOCATION"){
+            creneau.lieu = couple[1];
+        }
+    }
+}
+
+function Creneau(){
+    this.dateDébut;
+    this.dateFin;
+    this.titre;
+    this.lieu;
+}
